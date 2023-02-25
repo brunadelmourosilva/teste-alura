06b2ff18e532e32b88cfad06c63ad1eee5743ece
Bruna Delmouro <brunadelmouro@gmail.com>
2023-02-24 21:40:41 -0300

    adding Enroll class

--- school-main/src/main/java/br/com/alura/school/enroll/models/Enroll.java
+++ school-main/src/main/java/br/com/alura/school/enroll/models/Enroll.java
@@ -1,48 +1,80 @@
-package br.com.alura.school.enroll;
+package br.com.alura.school.enroll.models;
 
 import br.com.alura.school.course.Course;
 import br.com.alura.school.user.User;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.OffsetDateTime;
-
-import static javax.persistence.GenerationType.IDENTITY;
+import java.util.Objects;
 
 @Entity
-@Table(name = "ENROLL")
 public class Enroll {
 
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
+    @EmbeddedId
+    private EnrollCompositeId id;
 
-    private OffsetDateTime enrollDate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("courseId")
+    private Course course;
 
-//    private Course course;
-//
-//    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("userId")
+    private User user;
 
-    @Deprecated
-    protected Enroll() {
-        enrollDate = OffsetDateTime.now();
+    @Column(name = "created_on")
+    private OffsetDateTime createdOn;
+
+    public Enroll(){}
+
+    public Enroll(Course course, User user) {
+        this.course = course;
+        this.user = user;
+        this.id = new EnrollCompositeId(course.getId(), user.getId());
+        this.createdOn = OffsetDateTime.now();
     }
 
-    public Long getId() {
+    public EnrollCompositeId getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(EnrollCompositeId id) {
         this.id = id;
     }
 
-    public OffsetDateTime getEnrollDate() {
-        return enrollDate;
+    public Course getCourse() {
+        return course;
     }
 
-    public void setEnrollDate(OffsetDateTime enrollDate) {
-        this.enrollDate = enrollDate;
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public OffsetDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(OffsetDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Enroll enroll = (Enroll) o;
+        return Objects.equals(course, enroll.course) && Objects.equals(user, enroll.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course, user);
     }
 }
